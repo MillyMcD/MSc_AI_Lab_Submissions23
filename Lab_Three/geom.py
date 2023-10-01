08cee604c51d02f837939770800d94a5546f5d66
MillyMcD <millymcdowellart@gmail.com>
2023-10-01 20:58:31 +0100

    This is difficult, need to revise, testing Geom

--- Lab_Three/geom.py
+++ Lab_Three/geom.py
@@ -2,9 +2,9 @@
 class Rectangle: # rectangle class
     # make a rectangle using top left and bottom right coordinates
     def __init__(self,tl,br):
-        self.tl=tl
+        self.tl=tl #setting the variables
         self.br=br
-        self.width=abs(tl.x-br.x)  # width
+        self.width=abs(tl.x-br.x)  # width - this maths is new to me
         self.height=abs(tl.y-br.y) # height
     def area(self): # gets area of rectangle
         return self.width*self.height
@@ -19,3 +19,15 @@ class Coordinate: # coordinate class
         xdist=abs(self.x-another.x)
         ydist=abs(self.y-another.y)
         return math.sqrt(xdist**2+ydist**2) # pythagoras theorem
+     
+def test():
+    p1 = Coordinate(1,1)
+    p2 = Coordinate(4,5)
+    dist = p1.distance(p2)
+    print(f"I expect this to be 5, and it's actual distance is {dist}")
+
+    rec = Rectangle(p1,p2)
+    area = rec.area()
+    print(f"I expect this to be 12cm2, and it's actual area is {area}cm2")
+
+test()
--- Lab_Three/rectangle.py
+++ Lab_Three/rectangle.py
@@ -13,3 +13,24 @@ class Rectangle: # Rectangle class
     #perimeter method retuns the area of the rectangle
     def area(self):
         return self.__width * self.__height
+
+def test_area():
+    print("Testing area method")
+    rect = Rectangle(12,4)
+    actual = rect.area()
+    print("area of a rectangle of width 12 and height 4 is expected to be 48")
+    print(f"actual result {actual}")
+
+def test_perimeter():
+    print("Testing perimeter method")
+    rect = Rectangle (12,4)
+    actual = rect.perimeter()
+    print ("Perimeter of a rectangle of width 12 and height 4 is expected to be 32")
+    print (f'Actual result {actual}')
+
+def main():
+    test_area()
+    print("")
+    test_perimeter()
+
+main()
