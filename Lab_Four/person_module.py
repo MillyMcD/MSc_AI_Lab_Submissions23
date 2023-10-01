6c0e615b222ef4d59688a7f5395349e5c798ff99
MillyMcD <millymcdowellart@gmail.com>
2023-10-01 22:10:25 +0100

    Do exercise 3.1

--- Lab_Four/person_module.py
+++ Lab_Four/person_module.py
@@ -8,7 +8,6 @@ class Person:
 
 
 class Student(Person):
-
     def __init__(self, course, *args, **kwargs):
         self.course = course
         self.classes = []
@@ -19,14 +18,37 @@ class Student(Person):
 
 
 class StaffMember(Person):
-
-    def __init__(self, *args, **kwargs ):
+    def __init__(self, salary, *args, **kwargs ):
         self.courses = []
+        self.salary  = salary #salary was missing?
         super().__init__(*args, **kwargs)
 
     def administrator_for(self, module):
-        self.courses.append(course)
+        self.courses.append(module) #fixed this, should be `module`
 
-    def get_salary(self):
+    def get_salary(self): #now this is defined 
         return self.salary
+    
+
+class Lecturer(StaffMember):
+    def __init__(self,academic_title, *args, **kwargs ):
+        self.academic_title = academic_title
+        super().__init__(*args, **kwargs)
+    
+    def get_profile_info(self):
+        print(f'{self.academic_title} {self.name}, Phone Number: {self.phone_number}, Salary: {self.salary}')
+        for i in self.courses:
+            i.print_module_info()
+
+def main():
+    lec_one = Lecturer(academic_title='Dr',name='Dave Pillow',phone_number='07231231312',salary=45000)
+    lec_two = Lecturer(academic_title='Prof',name='Alice Berger',phone_number='0723122321',salary=70000)
+
+    lec_one.get_profile_info()
+    lec_two.get_profile_info()
+    
+
+main()
+
+
 
